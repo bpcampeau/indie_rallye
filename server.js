7b1385199a7ed67b111b6cf300298cf906d059d1
Bruno-Pierre Campeau <zerod.zunaro@gmail.com>
2018-07-04 09:28:36 -0400

    Added routes to add entries

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var entry      = require('./data/schema/entry');
-mongoose.connect('mongodb://127.0.0.1:27017');
-
+var Entry      = require('./data/schema/entry');
+mongoose.connect('mongodb://127.0.0.1:27017/indie_rally');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -23,6 +22,46 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.route('/entry')
+
+    // create a bear (accessed at POST http://localhost:8080/api/entry)
+    .post(function(req, res) {
+
+        var entry = new Entry();      // create a new instance of the Bear model
+        entry.indie_zone = req.body.indie_zone;
+        entry.indie_village = req.body.indie_village;
+        entry.name = req.body.name;
+        entry.lastName = req.body.lastName;
+        entry.phone = req.body.phone;
+        entry.email = req.body.email;
+        entry.favorite = req.body.favorite;
+        entry.email_consent = req.body.email_consent;
+        
+        // save the bear and check for errors
+        entry.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Entry created!' });
+        });
+
+    })
+
+    .get(function(req, res) {
+        Entry.find(function(err, entries) {
+            if (err)
+                res.send(err);
+
+            res.json(entries);
+        });
+    });
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
